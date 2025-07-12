97ff603423e75fef2c847196169cef7ac785f7c2
Mohamed Abdelhlim Saeed <MHalem_DEV@outlook.com>
2025-07-12 21:49:34 +0300

    Add Task 4

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,11 @@
+// File: task_4/js/subjects/Subject.ts
+/// <reference path="./Teacher.ts" />
+namespace Subjects {
+  export class Subject {
+    teacher: Teacher;
+
+    set setTeacher(teacher: Teacher) {
+      this.teacher = teacher;
+    }
+  }
+}
---
+++ 0x04-TypeScript/task_4/js/subjects/Teacher.ts
@@ -0,0 +1,7 @@
+// File: task_4/js/subjects/Teacher.ts
+namespace Subjects {
+  export interface Teacher {
+    firstName: string;
+    lastName: string;
+  }
+}
