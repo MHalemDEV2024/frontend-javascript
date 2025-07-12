56cff2e9cd49444424df73ada120e9031781e6f3
Mohamed Abdelhlim Saeed <MHalem_DEV@outlook.com>
2025-07-12 14:04:17 +0300

    Initial commit for task_1: Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,9 @@
+// 0x04-TypeScript/task_1/js/main.ts
+var teacher3 = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+};
+console.log(teacher3);
